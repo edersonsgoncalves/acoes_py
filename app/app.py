6fed0955b3c8bfc33289ce3adf7bd0a1a73cdd3a
edersonsgoncalves <edersonsgoncalves@gmail.com>
2025-09-22 17:47:41 -0300

    fix: implementar encapsulamento das variaveis de ambiente e do app. Registro do blueprint de ativos

--- app/app.py
+++ app/app.py
@@ -3,26 +3,42 @@ import os
 from dotenv import load_dotenv
 from models import db
 from routes.operacoes import bp_operacoes
+from routes.ativos import bp_ativos
 from routes.main import bp_inicio
-from flask_migrate import Migrate
+
+# from flask_migrate import Migrate
 
 
 load_dotenv()
 
-app = Flask(__name__)
 
-app.register_blueprint(bp_operacoes)
-app.register_blueprint(bp_inicio)
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.getenv("DB_URL")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    POLYGON_API_KEY = os.getenv("POLYGON_API_KEY")
+    ALPHA_VANTAGE_API_KEY = os.getenv("ALPHA_VANTAGE_API_KEY")
+    SERPAPI_API_KEY = os.getenv("SERPAPI_API_KEY")
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DB_URL")
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 
-migrate = Migrate(app, db)
-db.init_app(app)
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
 
-with app.app_context():
-    db.create_all()
+    # Registra seus Blueprints
+    app.register_blueprint(bp_operacoes)
+    app.register_blueprint(bp_ativos)
+    app.register_blueprint(bp_inicio)
+
+    # migrate = Migrate(app, db)  # type:ignore
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    return app
+
 
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True)
